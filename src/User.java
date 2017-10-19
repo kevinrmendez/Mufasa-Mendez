1bee0fbaca266aef3bc214c5956d75a501d82421
jhalkola <joona.halkola@hotmail.com>
2017-10-19 14:22:02 +0300

    second commit

--- src/User.java
+++ src/User.java
@@ -1,4 +1,7 @@
 
-public class User {
-
+public interface User {
+	public String getFirstName(String userID);
+	public String getLastName(String userID);
+	public int getPhoneNumber(String userID);
+	
 }
